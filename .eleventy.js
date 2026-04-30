f3929f8d390e3d1ca464f5efbc32267b244b9616
Hadleigh Jae Bills <hjb5471@psu.edu>
2026-04-30 11:22:21 -0400

    website CETEI and Eleventy test

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ module.exports = function (eleventyConfig){
     eleventyConfig.addPassthroughCopy("./_src/css/");
     eleventyConfig.addPassthroughCopy("./_src/img/");
     eleventyConfig.addPassthroughCopy("./_src/js/");
+    eleventyConfig.addPassthroughCopy("./_src/xml/");
+
+
 
     return{
         dir: {
---
+++ _src/voynich.md
@@ -0,0 +1,9 @@
+---
+title: Voynich
+layout: voynich.njk
+permalink: "voynich.html"
+---
+
+<header>
+<h1>{{ title }}</h1>
+</header>
